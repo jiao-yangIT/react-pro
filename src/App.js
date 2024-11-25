f419116b06e9d1a162a93913ace31720c71387dc
JoannaYang <13103860708@163.com>
2024-11-25 14:38:56 +0800

    feat: 组件状态（数据）

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react'
 import Home from "./components/Home";
 // import List from "./components/List";
 
+/*
+* 组件状态：状态即数据，因此组件状态指的是某一个组件自己的数据
+* 数据驱动DOM，当我们修改某一条数据时，界面上的DOM中数据展示也会自动更新
+* 组件状态管理
+* props的数据是只读的。
+ */
 function App() {
     return(
         <>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from "./Layout";
 
-function Home() {
-    return(
-        <>
-            <Layout>
-                <p>当前是home页面</p>
-            </Layout>
-        </>
-    )
+class Home extends Component {
+    //类组件中默认存在一个state属性，是一个对象，可以用于保存当前组件的数据
+    //之后还可以通过setState方法来修改数据的值，最后修改后的状态会自动显示在DOM界面上
+
+    state = {
+        name: 'YJJ',
+        age: 28
+    }
+
+    handleClick = ()=> {
+        this.setState({
+            name: this.state.name === 'LH' ? 'YJJ' : 'LH',
+            age: this.state.age === 26 ? 28 : 26
+        })
+    }
+    render() {
+        return(
+            <>
+                <Layout>
+                    <p>姓名：{ this.state.name}</p>
+                    <p>年龄：{ this.state.age}</p>
+                    <button onClick={this.handleClick}>点击改变数据</button>
+                </Layout>
+            </>
+        )
+    }
 }
 
+// function Home() {
+//     return(
+//         <>
+//             <Layout>
+//                 <p>当前是home页面</p>
+//             </Layout>
+//         </>
+//     )
+// }
+//
 export default Home
