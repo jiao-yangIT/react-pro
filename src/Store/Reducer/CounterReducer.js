459d7651e1e31a26bf2cdfc69b747389f6c3ee3d
JoannaYang <13103860708@163.com>
2024-12-19 16:17:05 +0800

    feat: 修改state的值及action 传递参数

--- src/Store/Reducer/CounterReducer.js
+++ src/Store/Reducer/CounterReducer.js
@@ -1,5 +1,21 @@
-export default ()=> {
-    return {
-        count: 10
+const initState = {
+    count: 6
+}
+export default (state = initState, action)=> {
+    switch (action.type) {
+        case 'add':
+            return {
+                count: state.count + 1
+            }
+        case 'add_w':
+            return {
+                count: state.count + action.payload
+            }
+        case 'adc':
+            return {
+                count: state.count -1
+            }
+        default:
+            return state
     }
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 function Counter(props) {
-    return(
-        <div>
-            <button>+1</button>
-            <span>{props.count}</span>
-            <button>-1</button>
-        </div>
-    )
+            return(
+                <div>
+                    {/*修改state的值时不直接修改，通过dispatch指令传递给reducer，再里面进行修改*/}
+                    <button onClick={()=> { props.dispatch({ type: 'add'})}}>+1</button>
+                    <button onClick={()=> { props.dispatch({ type: 'add_w', payload: 5})}}>+5</button>
+                    <span>{props.count}</span>
+                    <button onClick={()=> { props.dispatch({ type: 'adc'})}}>-1</button>
+                </div>
+            )
 }
 
 const mapStateToProps = state=> ({
